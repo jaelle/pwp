b2d3834a3f06888e3b33edcd57d24d1fa03d4698
Jaelle Scheuerman <jaelle@gmail.com>
2015-03-24 16:24:34 -0500

    Getting rid of extra folders

--- template/bootstrap.pl
+++
@@ -1,8 +0,0 @@
-hello("World").
-
-collapsible_group(1,"A Collapsible Group to test JavaScript",
-			"Anim pariatur cliche reprehenderit, enim eiusmod \c
- 				high life accusamus terry.").
-collapsible_group(2,"Another Collapsible Group",
-			"Anim pariatur cliche reprehenderit, enim eiusmod \c
-			high life accusamus terry.").
