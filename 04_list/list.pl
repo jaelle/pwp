1d888342b39ff75d322d22a0a0859bb6ac5c33d6
Jaelle Scheuerman <jaelle@gmail.com>
2015-03-30 23:20:34 -0500

    list example

---
+++ 04_list/list.pl
@@ -0,0 +1,20 @@
+favorite_thing(1,"raindrops on roses").
+favorite_thing(1,"whiskers on kittens").
+favorite_thing(1,"bright copper kettles").
+favorite_thing(1,"warm wollen mittens").
+favorite_thing(1,"brown paper bags tied up with strings").
+favorite_thing(2,"cream colored ponies").
+favorite_thing(2,"crisp apple strudels").
+favorite_thing(2,"doorbells").
+favorite_thing(2,"sleigh bells").
+favorite_thing(2,"schnitzel with noodles").
+favorite_thing(2,"wild geese that fly with the moon on their wings").
+favorite_thing(3,"girls in white dresses with blue satin sashes").
+favorite_thing(3,"snowflakes that stay on my nose and eyelashes").
+favorite_thing(3,"silver white winters that melt into springs").
+
+
+menu(3,"Contact","/contact").
+menu(1,"Home","/").
+menu(2,"Mission","/mission").
+menu(2,"About","/about").
