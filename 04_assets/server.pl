05d2a670e839b77261debf22f63ac53d43e4205a
jaelle <jaelle@gmail.com>
2015-04-01 04:58:37 +0000

    updated server for lesson 4

--- 04_assets/server.pl
+++ 04_assets/server.pl
@@ -7,9 +7,9 @@
 
 user:file_search_path(root,'.').
 
-:- http_handler('/js', assets_handler, [prefix, priority(1)]).
-:- http_handler('/css', assets_handler, [prefix, priority(1)]).
-:- http_handler('/images', assets_handler, [prefix, priority(1)]).
+:- http_handler('/js', assets_handler, [prefix]).
+:- http_handler('/css', assets_handler, [prefix]).
+:- http_handler('/images', assets_handler, [prefix]).
 
 % setup a handler to tell what prolog to do when the user visits anything under /
 :- http_handler(/, default_handler, []).
